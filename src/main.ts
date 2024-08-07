1f733e8a229016db196c4a387f02a476b30ee275
Stephanie Micah Cahutay <stephanie.m.cahutay@accenture.com>
2024-08-07 17:01:04 +0800

    Add app config

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { TasksComponent } from './app/tasks/tasks.component';
 import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { appConfig } from './app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter([
-    {
-      path: 'tasks' , //<your-domain/>tasks
-      component: TasksComponent,
-    }
-  ])]
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
