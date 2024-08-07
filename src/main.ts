ce766f27dae079bd831cfb84353a75c5fbe6fdf3
Stephanie Micah Cahutay <stephanie.m.cahutay@accenture.com>
2024-08-07 16:54:52 +0800

    Add router provider

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { TasksComponent } from './app/tasks/tasks.component';
+import { provideRouter } from '@angular/router';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter([
+    {
+      path: 'tasks' , //<your-domain/>tasks
+      component: TasksComponent,
+    }
+  ])]
+}).catch((err) => console.error(err));
