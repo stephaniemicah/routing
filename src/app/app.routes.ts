9e55dc1a7c14c8f11ba1d9582f8016f5054cbe29
Stephanie Micah Cahutay <stephanie.m.cahutay@accenture.com>
2024-08-08 13:51:53 +0800

    Add route for new tasks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from "@angular/router";
 import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 
 export const routes: Routes = [
   {
@@ -15,6 +16,10 @@ export const routes: Routes = [
       {
         path: 'tasks', // <your-domain>/users/<uid>/tasks
         component: TasksComponent
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent
       }
     ]
   },
