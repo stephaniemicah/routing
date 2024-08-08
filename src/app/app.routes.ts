07b78a0882e5d411d16582643f84446d9bde572d
Stephanie Micah Cahutay <stephanie.m.cahutay@accenture.com>
2024-08-08 15:15:25 +0800

    Add NotFound component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 export const routes: Routes = [
   {
@@ -23,4 +24,8 @@ export const routes: Routes = [
       }
     ]
   },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
 ];
