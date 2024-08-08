8cb573c6ee26bee91945142014f989c29fa48ca2
Stephanie Micah Cahutay <stephanie.m.cahutay@accenture.com>
2024-08-08 16:58:14 +0800

    Implement ngOnInit

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, computed, inject, input, OnInit } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
@@ -12,13 +12,20 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
   styleUrl: './tasks.component.css',
   imports: [TaskComponent, RouterLink],
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
   userId = input.required<string>();
-  order = input<'asc' | 'desc'>();
+  // order = input<'asc' | 'desc'>();
+  order?: 'asc' | 'desc';
   private tasksService = inject(TasksService);
   userTasks = computed(() =>
     this.tasksService.allTasks().filter((task) => task.userId === this.userId())
   );
 
   private activatedRoute = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe({
+      next: (params) => (this.order = params['order']),
+    })
+  }
 }
