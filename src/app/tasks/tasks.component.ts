fba0c7369f288738de52507ab03ced1ebb079653
Stephanie Micah Cahutay <stephanie.m.cahutay@accenture.com>
2024-08-08 14:31:58 +0800

    Inject TasksService in tasks component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,7 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -12,5 +13,6 @@ import { Task } from './task/task.model';
 })
 export class TasksComponent {
   userId = input.required<string>();
+  private tasksService = inject(TasksService);
   userTasks: Task[] = [];
 }
