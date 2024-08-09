68a637a2b2000b6277a090e7367d32f061212fd5
Stephanie Micah Cahutay <stephanie.m.cahutay@accenture.com>
2024-08-09 16:24:58 +0800

    Add navigate config and activated route

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -4,7 +4,7 @@ import { DatePipe } from '@angular/common';
 import { type Task } from './task.model';
 import { CardComponent } from '../../shared/card/card.component';
 import { TasksService } from '../tasks.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-task',
@@ -17,8 +17,13 @@ export class TaskComponent {
   task = input.required<Task>();
   private tasksService = inject(TasksService);
   private router = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
 
   onComplete() {
     this.tasksService.removeTask(this.task().id);
+    this.router.navigate(['./'], {
+      relativeTo: this.activatedRoute,
+      onSameUrlNavigation: 'reload',
+    });
   }
 }
